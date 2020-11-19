57d571b74c18c745660f4d8ea8dc759f43ec4d93
arunkulkarni95 <arunkulkarni95@hotmail.com>
2020-11-19 13:47:53 -0500

    add main method

---
+++ StudentDatabase/Main.java
@@ -0,0 +1,17 @@
+/* 
+Author: Arun Kulkarni 
+Date: March 10, 2019
+Description: This program creates a custom Java Swing GUI to create and store
+a student database. This class contains the main method and constructs a
+new instance of P4GUI.
+*/
+
+package p4gui;
+
+public class Main {
+    //begin main method
+    public static void main(String[] args) {
+        //new instance of P4GUI
+        new P4GUI();
+    }//end main method      
+}
